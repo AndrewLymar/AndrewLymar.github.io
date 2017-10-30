93a57f1bfd29d2d85c9d1cac13e3ceea4d98391f
AndrewLymar <andriilymar@gmail.com>
2017-10-30 13:50:06 +0200

    Update

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ var data = {
             codepen: "<a href='https://codepen.io/AndrewLymar' class='icon codepen' target='_blank'></a>",
             linkedin: "<a href='https://www.linkedin.com/in/andrey-lymar-91b145144/' class='icon linkedin' target='_blank'></a>"
         },
-        skills: "<span>My skills:</span><br> HTML5, CSS3, Bootstrap, JavaScript, Jquery, AJAX,<br> PHP, MySQL, CMS WordPress, Photoshop, PSD to HTML"
+        skills: "<span>My skills:</span><br> HTML, CSS, SCSS/Sass, Bootstrap, JavaScript, Jquery, AJAX, Gulp<br> PHP, MySQL, CMS WordPress, Photoshop, PSD to HTML"
     },
     footer: {
         copyright: "&#169; 2017 Andrey Lymar",
