0b678ceb847c4d992ec14f08c2bb2ca8de8c1124
AndrewLymar <andriilymar@gmail.com>
2019-03-16 13:11:29 +0200

    Update

--- src/js/data.js
+++ src/js/data.js
@@ -1,11 +1,11 @@
 var data = {
 	header: {
 		name: "Andrey Lymar",
-		specialization: "Front-end / Web Developer",
-		skills: "<span>My skills:</span><br> HTML, CSS, SCSS/Sass, Bootstrap, JavaScript, Jquery, Gulp<br> PHP, MySQL, CMS WordPress, Photoshop, PSD to HTML"
+		specialization: "Front-end Developer",
+		skills: "<span>My skills:</span><br> HTML, CSS, SCSS/Sass, Bootstrap, JavaScript, Jquery, Gulp<br> PHP, MySQL, CMS WordPress, Photoshop, Avocode"
 	},
 	footer: {
-		copyright: "&#169; 2018 Andrey Lymar",
+		copyright: "&#169; 2019 Andrey Lymar",
 	},
 	links: {
 		github: "<a href='https://github.com/AndrewLymar' target='_blank'><svg class = 'social-icon'><use xlink: href= 'img/sprite.svg#github'></use></svg ></a>",
