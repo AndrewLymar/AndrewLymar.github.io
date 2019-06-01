06f70be00af753785244c5bc7099aef603aca151
AndrewLymar <andriilymar@gmail.com>
2019-06-01 13:29:09 +0300

    Update

--- src/js/data.js
+++ src/js/data.js
@@ -8,8 +8,6 @@ var data = {
 		copyright: "&#169; 2019 Andrey Lymar",
 	},
 	links: {
-		github: "<a href='https://github.com/AndrewLymar' target='_blank'><svg class = 'social-icon'><use xlink: href= 'img/sprite.svg#github'></use></svg ></a>",
-		codepen: "<a href='https://codepen.io/AndrewLymar' target='_blank'><svg class = 'social-icon'><use xlink: href= 'img/sprite.svg#codepen'></use></svg ></a>",
-		linkedin: "<a href='https://www.linkedin.com/in/andrey-lymar-91b145144/' target='_blank'><svg class = 'social-icon'><use xlink: href= 'img/sprite.svg#linkedin'></use></svg ></a>"
+		github: "<a href='https://github.com/AndrewLymar' class='flex-row flex-middle' target='_blank'><svg class = 'social-icon'><use xlink: href= 'img/sprite.svg#github'></use></svg ><div class='profiles-text'>My github</div></a>"
 	}
 }
